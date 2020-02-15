75e36c0e3c2a209a3d6afbbab5c00f9623ff959c
Touza2el <59705964+Touza2el@users.noreply.github.com>
2020-02-15 13:04:56 +0100

    first version

---
+++ js/main.js
@@ -0,0 +1,96 @@
+// The Environement
+
+const slides = document.querySelector(".slider").children;
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+indicator = document.querySelector(".indicator");
+let index = 0;
+
+// Events
+
+prev.addEventListener("click", function() {
+  prevSlide();
+  updateCircleIndicator();
+  resetTimer();
+});
+next.addEventListener("click", function() {
+  nextSlide();
+  updateCircleIndicator();
+  resetTimer();
+});
+
+// Create Circle Indicators
+
+function circleIndicator() {
+  for (let i = 0; i < slides.length; i++) {
+    const div = document.createElement("div");
+    div.innerHTML = i + 1;
+    div.setAttribute("onclick", "indicateSlide(this)");
+    div.id = i;
+    if (i == 0) {
+      div.className = "active";
+    }
+    indicator.appendChild(div);
+  }
+}
+circleIndicator();
+function indicateSlide(element) {
+  index = element.id;
+  changeSlide();
+  updateCircleIndicator();
+  resetTimer();
+}
+
+function updateCircleIndicator() {
+  for (let i = 0; i < indicator.children.length; i++) {
+    indicator.children[i].classList.remove("active");
+  }
+  indicator.children[index].classList.add("active");
+}
+
+// Functions
+
+function prevSlide() {
+  if (index == 0) {
+    index = slides.length - 1;
+  } else {
+    index--;
+  }
+  // Call Function changeSlide()
+  changeSlide();
+}
+function nextSlide() {
+  if (index == slides.length - 1) {
+    index = 0;
+  } else {
+    index++;
+  }
+  // Call Function changeSlide()
+  changeSlide();
+}
+
+function changeSlide() {
+  // remove active classes from all slides
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+  }
+  slides[index].classList.add("active");
+}
+
+// AUto Play Function
+
+function autoPlay() {
+  nextSlide();
+  updateCircleIndicator();
+}
+let timer = setInterval(autoPlay, 4000);
+
+// Reset Timer
+
+function resetTimer() {
+  // when indicator or controls button clicked
+  // stop timer
+  clearInterval(timer);
+  // then started again timer
+  timer = setInterval(autoPlay, 4000);
+}
